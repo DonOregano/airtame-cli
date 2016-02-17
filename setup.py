f12f2056bb0627972de106d5cbac9030c3471f5a
Lars Hagström <lars@foldspace.nu>
2016-02-17 20:11:46 +0100

    packaging improvements

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 setup(name='airtame-cli',
       version='0.1',
-      py_modules=['airtame-cli'],
-      )
+      url='http://github.com/DonOregano/airtame-cli',
+      author='Lars Hagstrom',
+      author_email='lars@foldspace.nu',
+      license='GPLv3',
+      scripts=['airtame-cli'])
